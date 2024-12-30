77c8734528c634cd340689346ac5abc1999d924b
Es-sousy Mohammed <136990583+essousy11@users.noreply.github.com>
2024-12-30 18:09:47 +0100

    Update server.c

--- server.c
+++ server.c
@@ -25,8 +25,12 @@ SSL_CTX* create_context() {
 }
 
 void configure_context(SSL_CTX *ctx) {
-    if (SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <= 0 ||
-        SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0) {
+    if (SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0) {
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
@@ -41,29 +45,66 @@ int main() {
     configure_context(ctx);
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Unable to create socket");
+        exit(EXIT_FAILURE);
+    }
+
     addr.sin_family = AF_INET;
     addr.sin_port = htons(PORT);
     addr.sin_addr.s_addr = INADDR_ANY;
 
-    bind(sock, (struct sockaddr*)&addr, sizeof(addr));
-    listen(sock, 1);
+    if (bind(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("Unable to bind");
+        exit(EXIT_FAILURE);
+    }
 
-    printf("Server is listening on port %d...\n", PORT);
+    if (listen(sock, 1) < 0) {
+        perror("Unable to listen");
+        exit(EXIT_FAILURE);
+    }
 
-    int client = accept(sock, NULL, NULL);
-    SSL *ssl = SSL_new(ctx);
-    SSL_set_fd(ssl, client);
-    SSL_accept(ssl);
+    printf("Server is listening on port %d\n", PORT);
 
-    char buffer[1024] = {0};
-    SSL_read(ssl, buffer, sizeof(buffer));
-    printf("Client message: %s\n", buffer);
+    while (1) {
+        struct sockaddr_in client_addr;
+        uint len = sizeof(client_addr);
+        int client = accept(sock, (struct sockaddr*)&client_addr, &len);
+        if (client < 0) {
+            perror("Unable to accept connection");
+            continue;
+        }
 
-    SSL_write(ssl, "Hello from server!", strlen("Hello from server!"));
+        SSL *ssl = SSL_new(ctx);
+        SSL_set_fd(ssl, client);
 
-    SSL_shutdown(ssl);
-    SSL_free(ssl);
-    close(client);
+        if (SSL_accept(ssl) <= 0) {
+            ERR_print_errors_fp(stderr);
+        } else {
+            printf("Client connected successfully!\n");
+
+            char buffer[1024];
+            while (1) {
+                memset(buffer, 0, sizeof(buffer));
+                int bytes = SSL_read(ssl, buffer, sizeof(buffer));
+                if (bytes <= 0) break;
+
+                printf("Client: %s\n", buffer);
+
+                printf("Enter response to client: ");
+                fgets(buffer, sizeof(buffer), stdin);
+                buffer[strcspn(buffer, "\n")] = 0; // Remove newline character
+
+                SSL_write(ssl, buffer, strlen(buffer));
+            }
+        }
+
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
+        close(client);
+        printf("Client disconnected.\n");
+    }
+
     close(sock);
     SSL_CTX_free(ctx);
     EVP_cleanup();
